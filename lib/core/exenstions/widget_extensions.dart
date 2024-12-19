f0a37cd642305d694ced88e2dea27fda054bfeef
Maha Farghaly <139690305+mahafarghaly@users.noreply.github.com>
2024-12-19 10:53:32 +0200

    update widget_extensions

--- lib/core/exenstions/widget_extensions.dart
+++ lib/core/exenstions/widget_extensions.dart
@@ -49,29 +49,5 @@ extension WidgetModifier on Widget {
       child: this,
     );
   }
-
-
-  Widget background(Color color) {
-    return DecoratedBox(
-      decoration: BoxDecoration(
-        color: color,
-      ),
-      child: this,
-    );
-  }
-
-  Widget cornerRadius(BorderRadius radius) {
-    return ClipRRect(
-      borderRadius: radius,
-      child: this,
-    );
-  }
-
-  Widget align([AlignmentGeometry alignment = Alignment.center]) {
-    return Align(
-      alignment: alignment,
-      child: this,
-    );
-  }
 }
 
